b43acb70df45748b74acca80f9d899ce83abed1b
Hatajohn <35875766+Hatajohn@users.noreply.github.com>
2019-01-17 09:25:40 -0500

    011719_minor_update

--- ContraintTest/app/src/main/java/com/example/john/maptest/MainActivity.java
+++ ContraintTest/app/src/main/java/com/example/john/maptest/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.john.maptest;
 
 import android.Manifest;
-import android.location.LocationManager;
-import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -16,14 +14,15 @@ import android.net.wifi.WifiManager;
 import android.net.wifi.WifiInfo;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.util.TimingLogger;
 
 import 	android.os.Environment;
 
-import java.io.FileNotFoundException;
-import java.io.IOError;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -33,103 +32,91 @@ import android.media.MediaScannerConnection;
 
 
 public class MainActivity extends AppCompatActivity {
-    // Lat/Long values that correspond with the floor plans for reference
-    final double[] topLeft = {39.510926, -84.733822};
+    // Lat/Long values that correspond with the floor plans for reference; was more relevant during the first iteration.
+    /*final double[] topLeft = {39.510926, -84.733822};
     final double[] topRight = {39.510919, -84.733213};
     final double[] botLeft = {39.510617, -84.733829};
     final double[] botRight = {39.510609, -84.733221};
 
-    final double maxLat = 39.510926;
-    final double minLat = 39.510609;
+    final double maxLat = 39.510926;    //  this data was used in order to scale to a map interface before
+    final double minLat = 39.510609;    //  the application became more focused on gathering rssi values
     final double maxLong = -84.733829;
-    final double minLong = -84.733213;
+    final double minLong = -84.733213;*/
 
-    private final double c = 0.299792458;   // sol in nanoseconds
+    private Timer mTimer1;  //  originally opted to use threads to process scans, but it was not useful.
+    private TimerTask mTt1; //  timer tasks were an easier implementation for repeatedly running code.
+    private Handler mTimerHandler = new Handler();
 
-    private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11;
+    private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11; //  Application requires a number of permissions to run
     private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 11;
     private static final int MY_PERMISSION_ACCESS_WIFI_STATE = 11;
     private static final int MY_PERMISSION_WRITE_EXTERNAL_STORAGE = 11;
     private static final int MY_PERMISSION_READ_EXTERNAL_STORAGE = 11;
-    private Thread runner;
+    private int threadCount = 0;    // simple counter used to prevent more than one timer being run at a time
     private File f;
-    private int threadCount = 0;
+    private double start;   //  start time of scans
     private TimingLogger timer;
     private long timeStart;
     private long timeStop;
     private int written = 0;
-    private ArrayList<String> apMac = new ArrayList<String>();
+    private ArrayList<String> apMac = new ArrayList<String>();  //  data is stored using an ArrayList of ArrayLists
     private ArrayList<ArrayList<Integer>> aprssi = new ArrayList<ArrayList<Integer>>();
-    private int samples = 0;
+    private int samples = 0;    //  keeps track of number of samples collected during use of app
     public String bestProvider;
     private Context mContext;
 
-    public class runThread implements Runnable{
-        public runThread(){
-
-        }
-        public runThread(String threadName) {
-            runner = new Thread(this, threadName); // (1) Create a new thread.
-            System.out.println(runner.getName());
-            runner.start();
-        }
-
-        @Override
-        public void run() {
-            Looper.prepare();
-            if ( ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED
-                    && ( ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED )) {
-                LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-                long start = System.currentTimeMillis();
-                while (runner.isAlive()) {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
-                    WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-                    String ssidString = wifiInfo.getBSSID();
-                    TextView s = findViewById(R.id.filename);
-                    TextView runTime = findViewById(R.id.runTime);
-                    runTime.setText("" + (System.currentTimeMillis() - start)/60);
-                    if (isNetworkAvailable() && (System.currentTimeMillis() - start) < 60000) {
-                        getScan();
-                    }else if(written == 0){
-                        TextView ssidText = findViewById(R.id.ssid);
-                        File f1 = mContext.getExternalFilesDir(null);
-                        TextView m = findViewById(R.id.path);
-                        m.setText("" + f1.getPath());
-                        ssidText.setText("Current SSID: " + ssidString);
-                        if(!f.exists()){
-                            try {
-                                f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),((TextView)findViewById(R.id.filename)).getText().toString()+".txt");
-                                    PrintWriter p = new PrintWriter(f);
-                                    for(int i = 0; i < apMac.size(); i++){
-                                        p.print("<" + apMac.get(i) + "> : ");
-                                        for(int j : aprssi.get(i)){
-                                            p.print(j + ", ");
-                                        }
-                                        p.print("\n");
-                                    }
-                                    TextView path = findViewById(R.id.path);
-                                    path.setText(f.getAbsolutePath());
-                                    p.close();
-                                    written = 1;
-                                    MediaScannerConnection.scanFile(mContext, new String[]{f.toString()}, null, null);
-                                } catch(IOException e){
-                                    e.printStackTrace();
-                                }
-                        }
-                    }
-                }
+    private void stopTimer(){
+        if(mTimer1 != null){
+            if(written == 0){   //  if not written already will automatically attempt to save data
+                TextView ssidText = findViewById(R.id.ssid);    // gets current access point the device is connected to if connected.
+                File f1 = mContext.getExternalFilesDir(null);
+                TextView m = findViewById(R.id.path);   //  the path used is an internal storage space, which requires permissions
+                m.setText("" + f1.getPath());
+                saveData(); //  sets written to 1 in saveData when written automatically or manually
             }
+            mTimer1.cancel();   //  clears timers and resets the count so that new scans may be accomplished
+            mTimer1.purge();
+            threadCount = 0;
+        }
+    }
+
+    private void startTimer(){  //  Begins timer task for scanning
+        mTimer1 = new Timer();
+        mTt1 = new TimerTask() {
+            public void run() {
+                mTimerHandler.post(new Runnable() {
+                    public void run(){
+                        getScan();  //  scans access points in range of phone.
+                    }
+                });
+            }
+        };
+        mTimer1.schedule(mTt1, 0, 100); //  100ms delay to increase frequency of probes
+    }
+
+    private void saveData(){    //  method that write data to a .txt file when called
+        try {   //  attempts to write to a .txt file that can be parsed later and examined.
+            f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),((TextView)findViewById(R.id.filename)).getText().toString()+".txt");
+            PrintWriter p = new PrintWriter(f);
+            for(int i = 0; i < apMac.size(); i++){
+                p.print("<" + apMac.get(i) + "> : ");
+                for(int j : aprssi.get(i)){
+                    p.print(j + ", ");
+                }
+                p.print("\n");
+            }
+            TextView path = findViewById(R.id.path);    //  shows path to user
+            path.setText(f.getAbsolutePath());
+            p.close();
+            written = 1;    //  if file already exists the data will not be automatically saved when timer stops
+            MediaScannerConnection.scanFile(mContext, new String[]{f.toString()}, null, null);
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {  //  A variety of permissions are required to perform
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -160,27 +147,30 @@ public class MainActivity extends AppCompatActivity {
         }
         mContext = this;
 
-        Button button3 = findViewById(R.id.button3);
+        Button button3 = findViewById(R.id.scan);
         button3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 written = 0;
                 TextView path = findViewById(R.id.path);
                 f = mContext.getFilesDir();
                 path.setText("" + f.getPath());
-                if(threadCount < 1) { // max 2 threads
+                aprssi.clear();
+                apMac.clear();
+                TextView samples = findViewById(R.id.signalStr);
+                samples.setText("Results: " + 0);
+                if(threadCount < 1) { // max 1 timer
                     threadCount++;
-                    runThread thr = new runThread("locate");
+                    start = System.currentTimeMillis();
+                    startTimer();
                 }
                 // Code here executes on main thread after user presses button
             }
         });
 
         Button button5 = findViewById(R.id.save); // Up
-        button5.setOnClickListener(new View.OnClickListener() {
+        button5.setOnClickListener(new View.OnClickListener() { //  By pressing the "save" button the file can be overwritten.
             public void onClick(View v) {
-                TextView t = findViewById(R.id.filename);
-                f = new File(t.toString());
-                // Code here executes on main thread after user presses button
+                saveData();
             }
         });
     }
@@ -192,26 +182,40 @@ public class MainActivity extends AppCompatActivity {
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
-    private void getScan(){
-        if ( ContextCompat.checkSelfPermission( getApplicationContext(), Manifest.permission.ACCESS_WIFI_STATE ) == PackageManager.PERMISSION_GRANTED) {
-            WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
-            List<ScanResult> wifiList = wifiManager.getScanResults();
-            int count = 0;
-            TextView sig = findViewById(R.id.signalStr);
-            TextView ap = findViewById(R.id.ssidList);
-            for (ScanResult scanResult : wifiList) {
-                if(apMac.contains(scanResult.BSSID)){
-                    aprssi.get(apMac.indexOf(scanResult.BSSID)).add(scanResult.level);
-                }else{
-                    apMac.add(scanResult.BSSID);
-                    aprssi.add(new ArrayList<Integer>());
-                    aprssi.get(apMac.indexOf(scanResult.BSSID)).add(scanResult.level);
-                    count++;
+    private void getScan() {
+        TextView runTime = findViewById(R.id.runTime);
+        runTime.setText("" + (System.currentTimeMillis() - start)/1000);
+        if (System.currentTimeMillis() - start < 60000) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_WIFI_STATE) == PackageManager.PERMISSION_GRANTED) {
+                WifiManager wifiManager = null;
+                wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+                TextView ssidText = findViewById(R.id.ssid);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                String ssidString = wifiInfo.getBSSID();
+                ssidText.setText("Current SSID: " + ssidString);
+                List<ScanResult> wifiList = null;
+                wifiManager.startScan();
+                wifiList = wifiManager.getScanResults();
+
+                TextView sig = findViewById(R.id.signalStr);
+                TextView ap = findViewById(R.id.ssidList);
+
+                for (ScanResult scanResult : wifiList) {    //  This is the loop that accomplishes the scanning and stores in the ArrayList
+                    if (apMac.contains(scanResult.BSSID)) { //  Will add an ArrayList for a new AP or add to an existing one
+                        aprssi.get(apMac.indexOf(scanResult.BSSID)).add(scanResult.level);
+                    } else {
+                        apMac.add(scanResult.BSSID);
+                        aprssi.add(new ArrayList<Integer>());
+                        aprssi.get(apMac.indexOf(scanResult.BSSID)).add(scanResult.level);
+                    }
+                    samples++;  //  increases the number of samples
                 }
-                samples++;
+
+                ap.setText("APS: " + wifiList.size());
+                sig.setText("Total: " + apMac.size() + "; Samples: " + samples);
             }
-            ap.setText("APS: " + wifiList.size());
-            sig.setText("Current: " + count + "; Total: " + apMac.size() + "; Samples: " + samples);
+        } else {
+            stopTimer();
         }
     }
 
